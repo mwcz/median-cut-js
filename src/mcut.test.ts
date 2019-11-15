591977fb0b9c25ec40fcbe056d8f39226fe8c771
mwcz <mwc@clayto.com>
2019-11-15 15:48:56 -0500

    start conversion to ts

---
+++ src/mcut.test.ts
@@ -0,0 +1,361 @@
+import MCut from "./mcut";
+
+/*********************************************************************
+ *                          CUSTOM MATCHERS                          *
+ *********************************************************************/
+
+// beforeEach(function() {
+//   this.addMatchers({
+//     toBeA: function(type) {
+//       return this.actual instanceof type;
+//     },
+
+//     toBeEmpty: function() {
+//       return this.actual.length === 0;
+//     },
+
+//     toBeLength: function(ln) {
+//       return this.actual.length === ln;
+//     },
+
+//     toBeGreaterThanOrEqualTo: function(ln) {
+//       return this.actual >= ln;
+//     },
+
+//     toBeLessThanOrEqualTo: function(ln) {
+//       return this.actual <= ln;
+//     }
+//   });
+// });
+
+/*********************************************************************
+ *                        MCut API Definition                        *
+ *********************************************************************/
+
+describe("MCut", () => {
+  test("is defined", () => {
+    expect(MCut).toBeDefined();
+  });
+
+  // it("has init function", function() {
+  //   var mc = new MCut();
+  //   expect(mc.init).toBeA(Function);
+  // });
+
+  // it("has get_fixed_size_palette function", function() {
+  //   var mc = new MCut();
+  //   expect(mc.get_fixed_size_palette).toBeA(Function);
+  // });
+
+  // it("has get_dynamic_size_palette function", function() {
+  //   var mc = new MCut();
+  //   expect(mc.get_dynamic_size_palette).toBeA(Function);
+  // });
+});
+
+/*********************************************************************
+ *                   Individual MCut API functions                   *
+ *********************************************************************/
+
+// describe("MCut.get_fixed_size_palette", function() {
+//   it("handles empty data", function() {
+//     var mc = new MCut();
+//     mc.init([]);
+//     expect(mc.get_fixed_size_palette(0)).toBeLength(0);
+//     expect(mc.get_fixed_size_palette(1)).toBeLength(0);
+//     expect(mc.get_fixed_size_palette(2)).toBeLength(0);
+//     expect(mc.get_fixed_size_palette(3)).toBeLength(0);
+//     expect(mc.get_fixed_size_palette(4)).toBeLength(0);
+//   });
+
+//   it("handles a single data point", function() {
+//     var data = [[0, 0, 0]];
+//     var mc = new MCut();
+//     mc.init(data);
+//     expect(mc.get_fixed_size_palette(1)).toEqual([[0, 0, 0]]);
+//   });
+
+//   it("returns the requested size of palette", function() {
+//     var data = [
+//       [128, 142, 235],
+//       [105, 213, 55],
+//       [79, 50, 158],
+//       [224, 102, 218],
+//       [90, 206, 103],
+//       [61, 160, 121],
+//       [103, 233, 4],
+//       [132, 192, 50],
+//       [200, 204, 127],
+//       [215, 121, 176],
+//       [1, 225, 169],
+//       [8, 206, 217],
+//       [25, 195, 135],
+//       [193, 167, 83],
+//       [189, 62, 236],
+//       [71, 164, 43],
+//       [89, 68, 213],
+//       [17, 156, 150],
+//       [211, 3, 18],
+//       [23, 164, 153]
+//     ];
+//     var mc = new MCut();
+//     mc.init(data);
+//     expect(mc.get_fixed_size_palette(0)).toBeLength(0);
+//     expect(mc.get_fixed_size_palette(1)).toBeLength(1);
+//     expect(mc.get_fixed_size_palette(2)).toBeLength(2);
+//     expect(mc.get_fixed_size_palette(3)).toBeLength(3);
+//     expect(mc.get_fixed_size_palette(4)).toBeLength(4);
+//     expect(mc.get_fixed_size_palette(5)).toBeLength(5);
+//     expect(mc.get_fixed_size_palette(6)).toBeLength(6);
+//     expect(mc.get_fixed_size_palette(7)).toBeLength(7);
+//     expect(mc.get_fixed_size_palette(8)).toBeLength(8);
+//     expect(mc.get_fixed_size_palette(9)).toBeLength(9);
+//     expect(mc.get_fixed_size_palette(10)).toBeLength(10);
+//     expect(mc.get_fixed_size_palette(100)).toBeLength(100);
+//   });
+
+//   it("returns the same result when called several times", function() {
+//     var data = [
+//       [181, 156, 126],
+//       [215, 239, 132],
+//       [187, 153, 243],
+//       [209, 139, 195],
+//       [120, 112, 236],
+//       [6, 84, 188],
+//       [69, 72, 100],
+//       [41, 83, 117],
+//       [230, 228, 239],
+//       [199, 67, 44],
+//       [59, 240, 19],
+//       [27, 149, 165],
+//       [194, 106, 149],
+//       [171, 81, 210],
+//       [2, 50, 224],
+//       [27, 124, 141],
+//       [221, 107, 212],
+//       [128, 108, 118],
+//       [62, 159, 18],
+//       [111, 177, 17]
+//     ];
+//     var mc = new MCut();
+//     mc.init(data);
+//     expect(mc.get_fixed_size_palette(4)).toBeLength(4);
+//     expect(mc.get_fixed_size_palette(4)).toBeLength(4);
+//     expect(mc.get_fixed_size_palette(4)).toBeLength(4);
+//     expect(mc.get_fixed_size_palette(4)).toBeLength(4);
+//   });
+// });
+
+// describe("MCut.get_dynamic_size_palette", function() {
+//   it("handles empty data", function() {
+//     var mc = new MCut();
+//     mc.init([]);
+//     expect(mc.get_dynamic_size_palette()).toBeLength(0);
+//   });
+
+//   it("handles a single data point", function() {
+//     var data = [[0, 0, 0]];
+//     var mc = new MCut();
+//     mc.init(data);
+
+//     // low threshold
+//     expect(mc.get_dynamic_size_palette(0)).toEqual([[0, 0, 0]]);
+//     // high threshold
+//     expect(mc.get_dynamic_size_palette(1)).toEqual([[0, 0, 0]]);
+//   });
+
+//   it("returns the same result when called several times", function() {
+//     var data = [
+//       [181, 156, 126],
+//       [215, 239, 132],
+//       [187, 153, 243],
+//       [209, 139, 195],
+//       [120, 112, 236],
+//       [6, 84, 188],
+//       [69, 72, 100],
+//       [41, 83, 117],
+//       [230, 228, 239],
+//       [199, 67, 44],
+//       [59, 240, 19],
+//       [27, 149, 165],
+//       [194, 106, 149],
+//       [171, 81, 210],
+//       [2, 50, 224],
+//       [27, 124, 141],
+//       [221, 107, 212],
+//       [128, 108, 118],
+//       [62, 159, 18],
+//       [111, 177, 17]
+//     ];
+//     var mc = new MCut();
+//     mc.init(data);
+
+//     var result1 = mc.get_dynamic_size_palette(0.4);
+//     var result2 = mc.get_dynamic_size_palette(0.4);
+//     var result3 = mc.get_dynamic_size_palette(0.4);
+//     var result4 = mc.get_dynamic_size_palette(0.4);
+
+//     expect(result1).toEqual(result2);
+//     expect(result2).toEqual(result3);
+//     expect(result3).toEqual(result4);
+//   });
+
+//   it("returns more colors for higher threshold values", function() {
+//     var data = [
+//       [181, 156, 126],
+//       [215, 239, 132],
+//       [187, 153, 243],
+//       [209, 139, 195],
+//       [120, 112, 236],
+//       [6, 84, 188],
+//       [69, 72, 100],
+//       [41, 83, 117],
+//       [230, 228, 239],
+//       [199, 67, 44],
+//       [59, 240, 19],
+//       [27, 149, 165],
+//       [194, 106, 149],
+//       [171, 81, 210],
+//       [2, 50, 224],
+//       [27, 124, 141],
+//       [221, 107, 212],
+//       [128, 108, 118],
+//       [62, 159, 18],
+//       [111, 177, 17]
+//     ];
+//     var mc = new MCut();
+//     mc.init(data);
+
+//     var result1 = mc.get_dynamic_size_palette(0.0);
+//     var result2 = mc.get_dynamic_size_palette(0.3);
+//     var result3 = mc.get_dynamic_size_palette(0.6);
+//     var result4 = mc.get_dynamic_size_palette(0.9);
+//     var result5 = mc.get_dynamic_size_palette(1.0);
+
+//     expect(result5.length).toBeGreaterThanOrEqualTo(result4.length);
+//     expect(result4.length).toBeGreaterThanOrEqualTo(result3.length);
+//     expect(result3.length).toBeGreaterThanOrEqualTo(result2.length);
+//     expect(result2.length).toBeGreaterThanOrEqualTo(result1.length);
+//   });
+
+//   it("accepts typed arrays", function() {
+//     var data_uint32 = [
+//       new Uint32Array(181, 156, 126),
+//       new Uint32Array(215, 239, 132),
+//       new Uint32Array(187, 153, 243),
+//       new Uint32Array(209, 139, 195),
+//       new Uint32Array(120, 112, 236),
+//       new Uint32Array(6, 84, 188),
+//       new Uint32Array(69, 72, 100),
+//       new Uint32Array(41, 83, 117),
+//       new Uint32Array(230, 228, 239),
+//       new Uint32Array(199, 67, 44),
+//       new Uint32Array(59, 240, 19),
+//       new Uint32Array(27, 149, 165),
+//       new Uint32Array(194, 106, 149),
+//       new Uint32Array(171, 81, 210),
+//       new Uint32Array(2, 50, 224),
+//       new Uint32Array(27, 124, 141),
+//       new Uint32Array(221, 107, 212),
+//       new Uint32Array(128, 108, 118),
+//       new Uint32Array(62, 159, 18),
+//       new Uint32Array(111, 177, 17)
+//     ];
+//     var data_uint16 = [
+//       new Uint16Array(181, 156, 126),
+//       new Uint16Array(215, 239, 132),
+//       new Uint16Array(187, 153, 243),
+//       new Uint16Array(209, 139, 195),
+//       new Uint16Array(120, 112, 236),
+//       new Uint16Array(6, 84, 188),
+//       new Uint16Array(69, 72, 100),
+//       new Uint16Array(41, 83, 117),
+//       new Uint16Array(230, 228, 239),
+//       new Uint16Array(199, 67, 44),
+//       new Uint16Array(59, 240, 19),
+//       new Uint16Array(27, 149, 165),
+//       new Uint16Array(194, 106, 149),
+//       new Uint16Array(171, 81, 210),
+//       new Uint16Array(2, 50, 224),
+//       new Uint16Array(27, 124, 141),
+//       new Uint16Array(221, 107, 212),
+//       new Uint16Array(128, 108, 118),
+//       new Uint16Array(62, 159, 18),
+//       new Uint16Array(111, 177, 17)
+//     ];
+//     var data_uint8 = [
+//       new Uint8Array(181, 156, 126),
+//       new Uint8Array(215, 239, 132),
+//       new Uint8Array(187, 153, 243),
+//       new Uint8Array(209, 139, 195),
+//       new Uint8Array(120, 112, 236),
+//       new Uint8Array(6, 84, 188),
+//       new Uint8Array(69, 72, 100),
+//       new Uint8Array(41, 83, 117),
+//       new Uint8Array(230, 228, 239),
+//       new Uint8Array(199, 67, 44),
+//       new Uint8Array(59, 240, 19),
+//       new Uint8Array(27, 149, 165),
+//       new Uint8Array(194, 106, 149),
+//       new Uint8Array(171, 81, 210),
+//       new Uint8Array(2, 50, 224),
+//       new Uint8Array(27, 124, 141),
+//       new Uint8Array(221, 107, 212),
+//       new Uint8Array(128, 108, 118),
+//       new Uint8Array(62, 159, 18),
+//       new Uint8Array(111, 177, 17)
+//     ];
+//     var data_uint8c = [
+//       new Uint8ClampedArray(181, 156, 126),
+//       new Uint8ClampedArray(215, 239, 132),
+//       new Uint8ClampedArray(187, 153, 243),
+//       new Uint8ClampedArray(209, 139, 195),
+//       new Uint8ClampedArray(120, 112, 236),
+//       new Uint8ClampedArray(6, 84, 188),
+//       new Uint8ClampedArray(69, 72, 100),
+//       new Uint8ClampedArray(41, 83, 117),
+//       new Uint8ClampedArray(230, 228, 239),
+//       new Uint8ClampedArray(199, 67, 44),
+//       new Uint8ClampedArray(59, 240, 19),
+//       new Uint8ClampedArray(27, 149, 165),
+//       new Uint8ClampedArray(194, 106, 149),
+//       new Uint8ClampedArray(171, 81, 210),
+//       new Uint8ClampedArray(2, 50, 224),
+//       new Uint8ClampedArray(27, 124, 141),
+//       new Uint8ClampedArray(221, 107, 212),
+//       new Uint8ClampedArray(128, 108, 118),
+//       new Uint8ClampedArray(62, 159, 18),
+//       new Uint8ClampedArray(111, 177, 17)
+//     ];
+//     var mc = new MCut();
+
+//     mc.init(data_uint32);
+//     var result_uint32 = mc.get_dynamic_size_palette(0.0);
+
+//     mc.init(data_uint16);
+//     var result_uint16 = mc.get_dynamic_size_palette(0.0);
+
+//     mc.init(data_uint8);
+//     var result_uint8 = mc.get_dynamic_size_palette(0.0);
+
+//     mc.init(data_uint8c);
+//     var result_uint8c = mc.get_dynamic_size_palette(0.0);
+
+//     expect(result_uint32).toEqual([
+//       [44, 126, 109],
+//       [186, 136, 173]
+//     ]);
+//     expect(result_uint16).toEqual([
+//       [44, 126, 109],
+//       [186, 136, 173]
+//     ]);
+//     expect(result_uint8).toEqual([
+//       [44, 126, 109],
+//       [186, 136, 173]
+//     ]);
+//     expect(result_uint8c).toEqual([
+//       [44, 126, 109],
+//       [186, 136, 173]
+//     ]);
+//   });
+// });
---
+++ src/mcut.ts
@@ -0,0 +1,419 @@
+/**************************************************************************
+ * This file is part of median-cut.js                                     *
+ *                                                                        *
+ * median-cut.js is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or      *
+ * (at your option) any later version.                                    *
+ *                                                                        *
+ * median-cut.js is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ * GNU General Public License for more details.                           *
+ *                                                                        *
+ * You should have received a copy of the GNU General Public License      *
+ * along with median-cut.js.  If not, see <http://www.gnu.org/licenses/>. *
+ **************************************************************************/
+
+//  This is the median-cut algorithm.
+//
+//  1. Find the smallest box which contains all the colors in the image.
+//
+//  2. Sort the enclosed colors along the longest axis of the box.
+//
+//  3. Split the box into 2 regions at median of the sorted list.
+//
+//  4. Repeat the above process until the original color space has been divided
+//     into N regions where N is the number of colors you want.
+
+interface MinMax {
+  min: number;
+  max: number;
+}
+
+interface ComparisonFunction {
+  (a: number[], b: number[]): number
+}
+
+class Box {
+  // TODO: memoize all functions beginning with 'get_'.  Use for-in loop.
+  // get_longest_axis gets called twice now, and others may also.
+
+  data: number[]; // it's all about the data
+  box: MinMax[]; // the bounding box of the data
+  dim: number; // number of dimensions in the data
+
+  /** Initializes the data values, number of dimensions in the data (currently fixed to 3 to handle RGB, but may be
+   *  genericized in the future), and the bounding box of the data.
+   */
+  constructor(data: number[]) {
+
+    this.data = data;
+    this.dim = 3; // lock this to 3 (RGB pixels) for now.
+    this.box = this.calculate_bounding_box();
+  }
+
+  /**
+   * keeps running tally of the min and max values on each dimension initialize the min value to the highest number
+   * possible, and the max value to the lowest number possible
+   */
+  calculate_bounding_box() {
+
+    const minmax = [
+      { min: Number.MAX_VALUE, max: Number.MIN_VALUE },
+      { min: Number.MAX_VALUE, max: Number.MIN_VALUE },
+      { min: Number.MAX_VALUE, max: Number.MIN_VALUE }
+    ];
+
+    for (let i = this.data.length - 1; i >= 0; i -= 1) {
+      minmax[0].min = this.data[i][0] < minmax[0].min ? this.data[i][0] : minmax[0].min; // r
+      minmax[1].min = this.data[i][1] < minmax[1].min ? this.data[i][1] : minmax[1].min; // g
+      minmax[2].min = this.data[i][2] < minmax[2].min ? this.data[i][2] : minmax[2].min; // b
+
+      minmax[0].max = this.data[i][0] > minmax[0].max ? this.data[i][0] : minmax[0].max; // r
+      minmax[1].max = this.data[i][1] > minmax[1].max ? this.data[i][1] : minmax[1].max; // g
+      minmax[2].max = this.data[i][2] > minmax[2].max ? this.data[i][2] : minmax[2].max; // b
+    }
+
+    return minmax;
+  }
+
+  /**
+   * Returns the longest (aka 'widest') axis of the data in this box.
+   */
+  get_longest_axis() {
+
+    let longest_axis = 0;
+    let longest_axis_size = 0;
+    let axis_size = 0;
+
+    for (let i = this.dim - 1; i >= 0; i -= 1) {
+      axis_size = this.box[i].max - this.box[i].min;
+      if (axis_size > longest_axis_size) {
+        longest_axis = i;
+        longest_axis_size = axis_size;
+      }
+    }
+
+    return { axis: longest_axis, length: longest_axis_size };
+  }
+
+
+  /**
+   * Return a comparison function based on a given index (for median-cut, sort on the longest axis) ie: sort ONLY on a
+   * single axis.  get_comparison_func( 1 ) would return a sorting function that sorts the data according to each item's
+   * Green value.
+   */
+  get_comparison_func(_i: number): ComparisonFunction {
+
+    return (a, b) => a[_i] - b[_i];
+  }
+
+  /**
+   * Sorts all the elements in this box based on their values on the longest axis.
+   */
+  sort() {
+
+    const a = this.get_longest_axis().axis;
+    const sort_method = this.get_comparison_func(a);
+
+    Array.prototype.sort.call(this.data, sort_method);
+
+    return this.data;
+  }
+
+  /**
+   * Returns the position of the median value of the data in this box.  The position number is rounded down, to deal
+   * with cases when the data has an odd number of elements.
+   */
+  mean_pos() {
+
+    var mean_i;
+    var mean = 0;
+    var smallest_diff = Number.MAX_VALUE;
+    var axis = get_longest_axis().axis;
+    var diff;
+    var i;
+
+    // sum all the data along the longest axis...
+    for (i = data.length - 1; i >= 0; i -= 1) {
+      mean += data[i][axis];
+    }
+    mean /= data.length;
+
+    // find the data point that is closest to the mean
+    for (i = data.length - 1; i >= 0; i -= 1) {
+      diff = Math.abs(data[i][axis] - mean);
+      if (diff < smallest_diff) {
+        smallest_diff = diff;
+        mean_i = i;
+      }
+    }
+
+    // return the index of the data point closest to the mean
+
+    return mean_i;
+  }
+
+  function split() {
+    // Splits this box in two and returns two box objects. This function
+    // represents steps 2 and 3 of the algorithm, as written at the top
+    // of this file.
+
+    sort();
+
+    var med = mean_pos();
+    var data1 = Array.prototype.slice.call(data, 0, med); // elements 0 through med
+    var data2 = Array.prototype.slice.call(data, med); // elements med through end
+    var box1 = new Box();
+    var box2 = new Box();
+
+    box1.init(data1);
+    box2.init(data2);
+
+    return [box1, box2];
+  }
+
+  function average() {
+    // Returns the average value of the data in this box
+
+    var avg_r = 0;
+    var avg_g = 0;
+    var avg_b = 0;
+    var i;
+
+    for (i = data.length - 1; i >= 0; i -= 1) {
+      avg_r += data[i][0];
+      avg_g += data[i][1];
+      avg_b += data[i][2];
+    }
+
+    avg_r /= data.length;
+    avg_g /= data.length;
+    avg_b /= data.length;
+
+    return [parseInt(avg_r, 10), parseInt(avg_g, 10), parseInt(avg_b, 10)];
+  }
+
+  function median_pos() {
+    // Returns the position of the median value of the data in
+    // this box.  The position number is rounded down, to deal
+    // with cases when the data has an odd number of elements.
+
+    return Math.floor(data.length / 2);
+  }
+
+  function is_empty() {
+    // Self-explanatory
+
+    return data.length === 0;
+  }
+
+  function is_splittable() {
+    // A box is considered splittable if it has two or more items.
+
+    return data.length >= 2;
+  }
+
+  function get_bounding_box() {
+    // Getter for the bounding box
+    return box;
+  }
+
+  return {
+    /**/ // these are private functions
+    /**/
+    median_pos: median_pos,
+    get_bounding_box: get_bounding_box,
+    calculate_bounding_box: calculate_bounding_box,
+    sort: sort,
+    get_comparison_func: get_comparison_func,
+
+    // These are exposed (public) functions
+    mean_pos: mean_pos,
+    split: split,
+    is_empty: is_empty,
+    is_splittable: is_splittable,
+    get_longest_axis: get_longest_axis,
+    average: average,
+    init: init
+  };
+}
+
+function MCut() {
+  "use strict";
+
+  var boxes = [];
+  var data = [];
+
+  function init_boxes(_data) {
+    var succeeded = false;
+
+    if (is_valid_data(_data)) {
+      var box1 = new Box();
+      box1.init(_data);
+      boxes = [box1];
+      succeeded = true;
+    }
+
+    return succeeded;
+  }
+
+  function is_valid_data(_data) {
+    var has_length = _data.length > 0;
+
+    return has_length;
+  }
+
+  function init(_data) {
+    var boxes_init_success = init_boxes(_data);
+
+    if (boxes_init_success) {
+      data = _data;
+    }
+  }
+
+  function get_longest_box_index() {
+    // find the box with the longest axis of them all...
+    var longest_box_index = 0;
+    var box_index;
+
+    for (box_index = boxes.length - 1; box_index >= 0; box_index -= 1) {
+      if (boxes[box_index] > longest_box_index) {
+        longest_box_index = boxes[box_index];
+      }
+    }
+
+    return longest_box_index;
+  }
+
+  function get_boxes() {
+    return boxes;
+  }
+
+  function get_dynamic_size_palette(_threshold) {
+    // threshold is a value in (0,1] that influences how many colors
+    // will be in the resulting palette.  lower values of threshold
+    // will result in a smaller palette size.
+
+    var value;
+    var values;
+    var i;
+    var longest_box_index;
+    var longest_axis;
+    var min_box_length;
+    var box_to_split;
+    var split_boxes;
+    var box1;
+    var box2;
+
+    init_boxes(data);
+
+    // If there isn't any data, return early
+    if (boxes.length === 0) {
+      return [];
+    }
+
+    values = [];
+    longest_box_index = get_longest_box_index();
+    longest_axis = boxes[longest_box_index].get_longest_axis();
+
+    // a rough calculation of how big the palette should be
+    min_box_length = longest_axis.length * (1 - _threshold);
+
+    // but regardless of _threshold, the palette size should never
+    // exceed number of input data points
+
+    do {
+      // remove the longest box and split it
+      box_to_split = boxes.splice(longest_box_index, 1)[0];
+      split_boxes = box_to_split.split();
+
+      box1 = split_boxes[0];
+      box2 = split_boxes[1];
+
+      // then push the resulting boxes into the boxes array
+      boxes.push(box1);
+      boxes.push(box2);
+
+      longest_box_index = get_longest_box_index();
+      longest_axis = boxes[longest_box_index].get_longest_axis();
+    } while (longest_axis.length > min_box_length);
+
+    // palette is complete.  get the average colors from each box
+    // and push them into the values array, then return.
+    for (i = 0; i < boxes.length; i += 1) {
+      // check for NaN values (the results of splitting where no
+      // split should have been done)
+      // TODO fix NaNs
+      value = boxes[i].average();
+      if (!isNaN(value[0]) || !isNaN(value[0]) || !isNaN(value[0])) {
+        values.push(boxes[i].average());
+      }
+    }
+
+    return values;
+  }
+
+  function get_fixed_size_palette(_number) {
+    var values = [];
+    var i;
+    var longest_box_index;
+    var box_to_split;
+    var split_boxes;
+
+    init_boxes(data);
+
+    // If there isn't any data, return early
+    if (boxes.length === 0) {
+      return [];
+    }
+
+    for (i = _number - 1; i >= 0; i -= 1) {
+      longest_box_index = get_longest_box_index();
+
+      // remove the longest box and split it
+      box_to_split = boxes.splice(longest_box_index, 1)[0];
+
+      // TODO: If the box is large enough to be split, split it.
+      // Otherwise, push the box itself onto the boxes stack.  This is
+      // probably *non-desireable* behavior (i.e. it doesn't behave as
+      // the median cut algorithm should), but it's a side effect of
+      // requiring a fixed size palette.
+
+      if (box_to_split.is_splittable()) {
+        // split the box and push both new boxes
+        split_boxes = box_to_split.split();
+        boxes.push(split_boxes[0]);
+        boxes.push(split_boxes[1]);
+      } else {
+        // else... the box is too small to be split.  Push it into the
+        // set of boxes twice in order to guarantee the fixed-size
+        // palette.
+        boxes.push(box_to_split);
+        boxes.push(box_to_split);
+      }
+    }
+
+    // palette is complete.  get the average colors from each box
+    // and push them into the values array, then return.
+    for (i = _number - 1; i >= 0; i -= 1) {
+      values.push(boxes[i].average());
+    }
+
+    return values;
+  }
+
+  return {
+    // This is a private function (listed here in case it needs to be made
+    // public easily :)
+
+    // These are exposed (public) functions
+    get_boxes: get_boxes,
+    init: init,
+    get_fixed_size_palette: get_fixed_size_palette,
+    get_dynamic_size_palette: get_dynamic_size_palette
+  };
+}
+
+export default MCut;
